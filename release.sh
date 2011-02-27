51bd7524bef77d4c676bc7b109e6112bcc804aa6
Davy Verstappen <davy.verstappen@gmail.com>
2011-02-27 17:33:57 +0100

    Set up distributionManagement as described in:
    http://cemerick.com/2010/08/24/hosting-maven-repos-on-github/

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@
 # http://cemerick.com/2010/08/24/hosting-maven-repos-on-github/
 
 mvn --batch-mode release:prepare
-mvn -DaltDeploymentRepository=dverstap-github::default::file:../maven-repo/releases release:perform
+mvn -DaltDeploymentRepository=dverstap-github-releases::default::file:../maven-repo/releases release:perform
