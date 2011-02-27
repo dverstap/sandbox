37154b52cbae56bf0f3e5074d2a5437ec0c6c584
Davy Verstappen <davy.verstappen@gmail.com>
2011-02-27 17:59:17 +0100

    More hacks to make maven-release-plugin work with -DaltDeploymentRepository

--- release.sh
+++ release.sh
@@ -4,4 +4,11 @@
 # http://cemerick.com/2010/08/24/hosting-maven-repos-on-github/
 
 mvn --batch-mode release:prepare
+
+# the release build will actually run in target/checkout,
+# so let's clone the maven repo at target/checkout/../maven-repo :
+cd target
+git clone git@github.com:dverstap/maven-repo.git
+cd ..
+
 mvn -DaltDeploymentRepository=dverstap-github-releases::default::file:../maven-repo/releases release:perform
